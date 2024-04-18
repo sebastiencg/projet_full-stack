227e76b3cd68737d3fdb8277951a1d28d5d03e8d
Ttst <thibautstachnick@gmail.com>
2024-04-18 15:36:56 +0200

    add map to home component

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,13 +1,40 @@
-<script lang="ts">
-
-</script>
-
 <template>
-  <div class="div">
-    <h1>Hello HomeComponent</h1>
+  <div style="height:90vh;">
+    <l-map ref="map" v-model:zoom="zoom" :center="[45.75, 5.15]">
+      <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+  <div class="eventList">
+
   </div>
 </template>
 
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 10.3,
+    };
+  },
+};
+</script>
+
 <style scoped>
+.eventList{
+  height: 30vh;
+  width: 100%;
+  background-color: black;
+}
 
 </style>
